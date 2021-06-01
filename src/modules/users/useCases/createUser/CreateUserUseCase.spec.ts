78c5663addc24c9ca92393b4df2ba07cf5bee16d
unknown <22guigs@gmail.com>
2021-06-01 09:00:06 -0400

    chore: add eslint and prettier dependencies

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,1 +1,27 @@
-describe("Create User", () => { console})
+import { InMemoryUsersRepository } from "modules/users/repositories/in-memory/InMemoryUsersRepository";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Create User", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it("Should de able to create a new user.", async () => {
+    const user = {
+      name: "User Test",
+      email: "test@email.com",
+      password: "123test",
+    };
+
+    await createUserUseCase.execute({
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    });
+  });
+});
